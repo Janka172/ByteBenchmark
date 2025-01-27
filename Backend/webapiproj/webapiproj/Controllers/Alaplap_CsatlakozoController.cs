4a6b80a034d60b310f8fdb348b5f6fa1cd5f3e0a
szabomate2005 <mateszabo9784@gmail.com>
2025-01-27 09:55:10 +0100

    0127
    AlaplapCsatlakozot át kell irni, mert a viktor szerint nem jó. Külön post és delete kell.

--- Backend/webapiproj/webapiproj/Controllers/Alaplap_CsatlakozoController.cs
+++ Backend/webapiproj/webapiproj/Controllers/Alaplap_CsatlakozoController.cs
@@ -15,8 +15,9 @@ namespace webapiproj.Controllers
         public string AlaplapNev { get; set; }
         public string CsatlakozoNev { get; set; }
     }
-    public class AlaplapCsatlakozPutModel
+    public class AlaplapCsatlakozPOSTModel
     {
+        public string AlaplapNev { get; set; }
         public List<string> Csatlakozok { get; set; }
     }
     public class Alaplap_CsatlakozokController : ApiController
@@ -50,68 +51,43 @@ namespace webapiproj.Controllers
 
         // POST api/<controller>
         [ResponseType(typeof(AlaplapModel))]
-        public void Post([FromBody] string value)
+        public HttpResponseMessage Post([FromBody] AlaplapCsatlakozPOSTModel value)
         {
-        }
-
-        // PUT api/<controller>/5
-        [ResponseType(typeof(AlaplapModel))]
-        public HttpResponseMessage Put(int id,string name, [FromBody] AlaplapCsatlakozPutModel value)
-        {
+            var AlaplapId = ctx.Alaplapok.Where(x => x.Nev == value.AlaplapNev).Select(x => x.Id).FirstOrDefault();
+            if (AlaplapId == -1) return Request.CreateResponse(HttpStatusCode.NotFound, "Nem található ilyen alaplap");
+            List<int> A = new List<int>();
+            
             try
             {
-                var AlaplapId = -1;
-                AlaplapId = ctx.Alaplapok.Where(x => x.Nev == name).Select(x => x.Id).FirstOrDefault();
-                if (AlaplapId == -1) return Request.CreateResponse(HttpStatusCode.NotFound, "Nem található ilyen alaplap");
 
-                var aktcsatlakozo = ctx.Alaplap_Csatlakozok.Where(x => x.AlaplapId == AlaplapId).ToList(); //aktuális csatlakozok (update elott)
-                var osszcsatlakozo = ctx.Csatlakozok.Where(x=>value.Csatlakozok.Contains(x.Nev)).ToList(); //azokat listázza ki, amelyikeket a mi általunk beirt listábol szerepelnek
-                if (value.Csatlakozok.Count() != osszcsatlakozo.Count()) return Request.CreateResponse(HttpStatusCode.NotFound, "Vannak olyan ön által beirt csatlakozok, amik nem szerepelnek a csatlakozo listában"); //ha kevesebb a visszakapott érték hossza mint a lista hossza akkor van olyan ami nem szerepel a csatlakozo listában
-
-                try
+                foreach (var item in value.Csatlakozok)
                 {
-                    foreach (var item in aktcsatlakozo)  //regi kapcsolat torles ha mér nem szerepel a felhasználó által megadott listában
+                    A.Add(ctx.Csatlakozok.Where(x => x.Nev == item).Select(x => x.Id).FirstOrDefault());
+                }
+                foreach (var item in A)
+                {
+                    ctx.Alaplap_Csatlakozok.Add(new Alaplap_Csatlakozo
                     {
-                        if (!osszcsatlakozo.Any(x => x.Id == item.CsatlakozoId)) //Any azt vizsgéja, hogy bármelyik elem megfelel-e az adott feltételnek.
-                        {
-                            ctx.Alaplap_Csatlakozok.Remove(item);
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-
-                    return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
-                }
-
-                try
-                {
-                    foreach (var item in osszcsatlakozo)   //uj kapcsolat létrehozása.
-                    {
-                        if (!aktcsatlakozo.Any(x => x.CsatlakozoId == item.Id)) //Any azt vizsgéja, hogy bármelyik elem megfelel-e az adott feltételnek.
-                        {
-                            ctx.Alaplap_Csatlakozok.Add(new Alaplap_Csatlakozo
-                            {
-                                AlaplapId = AlaplapId,
-                                CsatlakozoId = item.Id
-                            });
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-
-                    return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+                        AlaplapId = AlaplapId,
+                        CsatlakozoId = item
+                    });
                 }
 
                 ctx.SaveChanges();
-                return Request.CreateResponse(HttpStatusCode.OK, "sikeres update");
+                return Request.CreateResponse(HttpStatusCode.OK, "Sikeres feltoltes");
             }
-            catch ( Exception ex)
+            catch (Exception ex)
             {
 
                 return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
             }
+        }
+
+        // PUT api/<controller>/5
+        [ResponseType(typeof(AlaplapModel))]
+        public void Put(int id,string name, [FromBody] string value)
+        {
+            
         }
 
         // DELETE api/<controller>/5
