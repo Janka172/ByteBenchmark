8f97ea726e51bffd8939e4bd231d2dd06553b7ec
szabomate2005 <mateszabo9784@gmail.com>
2025-02-05 18:03:09 +0100

    0205
    Applikacio post

--- Backend/webapiproj/webapiproj/Controllers/ApplikacioController.cs
+++ Backend/webapiproj/webapiproj/Controllers/ApplikacioController.cs
@@ -15,6 +15,7 @@ namespace webapiproj.Controllers
         public string Nev { get; set; }
         public string KategoriaNev { get; set; }
         public string KepeleresiUtja { get; set; }
+        public int Tarhely { get; set; }
 
     }
     public class ApplikacioController : ApiController
@@ -29,7 +30,8 @@ namespace webapiproj.Controllers
             {
                 Nev = x.Nev,
                 KategoriaNev = x.Kategoria.Nev,
-                KepeleresiUtja = x.Kepeleresiutja
+                KepeleresiUtja = x.Kepeleresiutja,
+                Tarhely=x.Tarhely
 
             }).ToList();
             return Request.CreateResponse(HttpStatusCode.OK, result);
@@ -44,14 +46,34 @@ namespace webapiproj.Controllers
             {
                 Nev = x.Nev,
                 KategoriaNev = x.Kategoria.Nev,
-                KepeleresiUtja = x.Kepeleresiutja
+                KepeleresiUtja = x.Kepeleresiutja,
+                Tarhely = x.Tarhely
             }).FirstOrDefault();
             return Request.CreateResponse(HttpStatusCode.OK, result);
         }
 
         // POST api/<controller>
-        public void Post([FromBody] string value)
+        public IHttpActionResult Post([FromBody] ApplikacioModel value)
         {
+            var katId = ctx.Kategoriak.Where(x => x.Nev == value.KategoriaNev).Select(x => x.Id).FirstOrDefault();
+            try
+            {
+                var result = ctx.Applikaciok.Add(new Applikacio
+                {
+                    Nev=value.Nev,
+                    Tarhely=value.Tarhely,
+                    KatId=katId,
+                    Kepeleresiutja=value.KepeleresiUtja
+                });
+                ctx.SaveChanges();
+
+                return Created($"api/Applikacio/{result}", result);
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message == "An error occurred while updating the entries. See the inner exception for details.") return Content(HttpStatusCode.Conflict, "már szerepel ez az applikáció");
+                return BadRequest("Applikacio feltoltese sikertelen");
+            }
         }
 
         // PUT api/<controller>/5
