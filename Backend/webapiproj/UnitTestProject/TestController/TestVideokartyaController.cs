52567411168c618be8ba2f010af600b15abfada5
szabomate2005 <mateszabo9784@gmail.com>
2025-02-06 13:46:53 +0100

    0206
    unittestvideokartya nem müködik, de a program futtatható

--- Backend/webapiproj/UnitTestProject/TestController/TestVideokartyaController.cs
+++ Backend/webapiproj/UnitTestProject/TestController/TestVideokartyaController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using webapiproj.Models;
 using webapiproj.Controllers;
+using System.Web.Http;
+using System.Web.Http.Results;
 
 namespace UnitTestProject.TestController
 {
@@ -17,12 +19,22 @@ namespace UnitTestProject.TestController
            ctx.Videokartyak.Add(
                new Videokartya { Id = 1, Nev="Demo1", AlaplapiCsatlakozas="Pci1",AjanlottTapegyseg=500,MonitorCsatlakozas="Valami,Valami2",ChipGyartoja="NVIIDA",Vram=5});
             ctx.Videokartyak.Add(
-              new Videokartya { Id = 1, Nev = "Demo2", AlaplapiCsatlakozas = "Pci2", AjanlottTapegyseg = 400, MonitorCsatlakozas = "Valami,Valami2", ChipGyartoja = "MAS", Vram = 9 });
+              new Videokartya { Id = 2, Nev = "Demo2", AlaplapiCsatlakozas = "Pci2", AjanlottTapegyseg = 400, MonitorCsatlakozas = "Valami,Valami2", ChipGyartoja = "MAS", Vram = 9 });
             ctx.Videokartyak.Add(
-              new Videokartya { Id = 1, Nev = "Demo3", AlaplapiCsatlakozas = "Pci3", AjanlottTapegyseg = 550, MonitorCsatlakozas = "Valami,Valami2", ChipGyartoja = "NVIIDA", Vram = 5 });
-
+              new Videokartya { Id = 3, Nev = "Demo3", AlaplapiCsatlakozas = "Pci3", AjanlottTapegyseg = 550, MonitorCsatlakozas = "Valami,Valami2", ChipGyartoja = "NVIIDA", Vram = 5 });
+            ctx.SaveChanges();
         }
-       //[TestMethod]
+       [TestMethod]
+       public void Get_OsszVideokartya()
+       {
+            var ctx = new TestProjektContext();
+            FillTestDatabase(ctx);
+            var controller = new VideokartyaController(ctx);
+            var result = controller.Get() as OkNegotiatedContentResult<List<VideokartyaModel>>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Content.Count);
+       }
+   }
        //public void Get_ShouldReturnAllCategories()
        //{
        //    var context = new TestProjektContext();
@@ -30,79 +42,79 @@ namespace UnitTestProject.TestController
        //    context.Videokartyak.Add(new Category { Id = 2, Name = "Demo2" });
        //    context.Videokartyak.Add(new Category { Id = 3, Name = "Demo3" });
 
-       //    var controller = new CategoriesController(context);
-       //    var result = controller.Get();
+        //    var controller = new CategoriesController(context);
+        //    var result = controller.Get();
 
-       //    Assert.IsNotNull(result);
-       //    Assert.AreEqual(3, result.Count());
-       //}
+        //    Assert.IsNotNull(result);
+        //    Assert.AreEqual(3, result.Count());
+        //}
 
-       //[TestMethod]
-       //public void Get_ShouldReturnSameCategory()
-       //{
-       //    var ctx = new TestMenuContext();
-       //    ctx.Categories.Add(GetDemoCategory());
+        //[TestMethod]
+        //public void Get_ShouldReturnSameCategory()
+        //{
+        //    var ctx = new TestMenuContext();
+        //    ctx.Categories.Add(GetDemoCategory());
 
-       //    var controller = new CategoriesController(ctx);
-       //    var result = controller.Get(1);
+        //    var controller = new CategoriesController(ctx);
+        //    var result = controller.Get(1);
 
-       //    Assert.IsNotNull(result);
-       //    Assert.AreEqual(1, result.Id);
-       //}
+        //    Assert.IsNotNull(result);
+        //    Assert.AreEqual(1, result.Id);
+        //}
 
-       //[TestMethod]
-       //public void Post_ShoulReturnSameCategory()
-       //{
-       //    var controller = new CategoriesController(new TestMenuContext());
+        //[TestMethod]
+        //public void Post_ShoulReturnSameCategory()
+        //{
+        //    var controller = new CategoriesController(new TestMenuContext());
 
-       //    var category = GetDemoCategory();
-       //    controller.Post(new CategoryModel { Name = category.Name });
+        //    var category = GetDemoCategory();
+        //    controller.Post(new CategoryModel { Name = category.Name });
 
-       //    var res = controller.Get().Last();
+        //    var res = controller.Get().Last();
 
-       //    Assert.IsNotNull(res);
-       //    Assert.AreEqual("Demo", res.Name);
-       //}
+        //    Assert.IsNotNull(res);
+        //    Assert.AreEqual("Demo", res.Name);
+        //}
 
-       //[TestMethod]
-       //public void Put_SuccessfulUpdate()
-       //{
-       //    var ctx = new TestMenuContext();
-       //    var controller = new CategoriesController(ctx);
-       //    ctx.Categories.Add(new Category { Id = 0, Name = "Demo0" });
-       //    ctx.Categories.Add(new Category { Id = 1, Name = "Demo1" });
-       //    ctx.Categories.Add(new Category { Id = 2, Name = "Demo2" });
+        //[TestMethod]
+        //public void Put_SuccessfulUpdate()
+        //{
+        //    var ctx = new TestMenuContext();
+        //    var controller = new CategoriesController(ctx);
+        //    ctx.Categories.Add(new Category { Id = 0, Name = "Demo0" });
+        //    ctx.Categories.Add(new Category { Id = 1, Name = "Demo1" });
+        //    ctx.Categories.Add(new Category { Id = 2, Name = "Demo2" });
 
-       //    controller.Put(1, new CategoryModel { Name = "UpdatedDemo1" });
+        //    controller.Put(1, new CategoryModel { Name = "UpdatedDemo1" });
 
-       //    var res1 = ctx.Categories.Where(x => x.Id == 1).FirstOrDefault();
-       //    var res2 = ctx.Categories.Where(x => x.Name == "UpdatedDemo1");
+        //    var res1 = ctx.Categories.Where(x => x.Id == 1).FirstOrDefault();
+        //    var res2 = ctx.Categories.Where(x => x.Name == "UpdatedDemo1");
 
-       //    Assert.IsNotNull(res1);
-       //    Assert.IsNotNull(res2);
-       //    Assert.AreEqual(1, res2.Count());
-       //    Assert.AreEqual("UpdatedDemo1", res1.Name);
-       //}
+        //    Assert.IsNotNull(res1);
+        //    Assert.IsNotNull(res2);
+        //    Assert.AreEqual(1, res2.Count());
+        //    Assert.AreEqual("UpdatedDemo1", res1.Name);
+        //}
 
-       //[TestMethod]
-       //public void Delete_SuccessfulDelete()
-       //{
-       //    var ctx = new TestMenuContext();
-       //    var controller = new CategoriesController(ctx);
-       //    ctx.Categories.Add(new Category { Id = 1, Name = "Demo1" });
-       //    ctx.Categories.Add(new Category { Id = 2, Name = "Demo2" });
+        //[TestMethod]
+        //public void Delete_SuccessfulDelete()
+        //{
+        //    var ctx = new TestMenuContext();
+        //    var controller = new CategoriesController(ctx);
+        //    ctx.Categories.Add(new Category { Id = 1, Name = "Demo1" });
+        //    ctx.Categories.Add(new Category { Id = 2, Name = "Demo2" });
 
-       //    controller.Delete(1);
-       //    var res1 = ctx.Categories.Where(x => x.Id == 1).FirstOrDefault();
-       //    var res2 = ctx.Categories.ToList();
+        //    controller.Delete(1);
+        //    var res1 = ctx.Categories.Where(x => x.Id == 1).FirstOrDefault();
+        //    var res2 = ctx.Categories.ToList();
 
-       //    Assert.IsNull(res1);
-       //    Assert.AreEqual(1, res2.Count);
-       //}
+        //    Assert.IsNull(res1);
+        //    Assert.AreEqual(1, res2.Count);
+        //}
 
-       //public Category GetDemoCategory()
-       //{
-       //    return new Category { Id = 1, Name = "Demo" };
-       //}
-   }
+        //public Category GetDemoCategory()
+        //{
+        //    return new Category { Id = 1, Name = "Demo" };
+        //}
+    //}
 }
