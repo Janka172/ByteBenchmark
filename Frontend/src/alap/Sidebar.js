999feaa3c3f325ff2fdd5f7f226f267d5aa86f6c
Janka172 <jankahegedus872@gmail.com>
2025-02-06 11:00:20 +0100

    +semmi
    nem jó a:
    - bejelentkezés
    - alaplap
    getek

--- Frontend/src/alap/Sidebar.js
+++ Frontend/src/alap/Sidebar.js
@@ -15,15 +15,28 @@ function Sidebar() {
 
   const kicsie = window.innerWidth <= 767;
 
+  function kivalaszt(azon){
+    const navigElems = document.getElementsByClassName('navigElem');
+    for (let i = 0; i < navigElems.length; i++) {
+      navigElems[i].style.backgroundColor = '';
+    }
+
+    try {
+      document.getElementById(azon).style.backgroundColor = 'white';
+    } catch {
+      console.error();
+    }
+  }
+
   return (
     <div>
       <div className="sidebar blockSidebar balAnim" style={{ width: sidebarOpen ? (kicsie ? '40%' : '25%') : '0', display: sidebarOpen ? 'block' : 'none',}} id="mySidebar">
         <button className="navigElem gombi visszaGomb" onClick={close}>Vissza &times;</button>
-        <Link to="/oldalak/Alkalmazasok" className="navigElem gombi">Alkalmazások</Link>
-        <Link to="/oldalak/Alkatreszek" className="navigElem gombi">Alkatrészek</Link>
-        <Link to="/oldalak/SajatSetup" className="navigElem gombi">Saját setup</Link>
-        <Link to="/oldalak/UjAlkalmazas" className="navigElem gombi">Új alkalmazás</Link>
-        <Link to="/oldalak/UjAlkatresz" className="navigElem gombi">Új alkatrész</Link>
+        <Link to="/oldalak/Alkalmazasok" className="navigElem gombi" id='L1' onClick={() => kivalaszt('L1')}>Alkalmazások</Link>
+        <Link to="/oldalak/Alkatreszek" className="navigElem gombi" id='L2' onClick={() => kivalaszt('L2')}>Alkatrészek</Link>
+        <Link to="/oldalak/SajatSetup" className="navigElem gombi" id='L3' onClick={() => kivalaszt('L3')}>Saját setup</Link>
+        <Link to="/oldalak/UjAlkalmazas" className="navigElem gombi" id='L4' onClick={() => kivalaszt('L4')}>Új alkalmazás</Link>
+        <Link to="/oldalak/UjAlkatresz" className="navigElem gombi" id='L5' onClick={() => kivalaszt('L5')}>Új alkatrész</Link>
       </div>
 
       <div id="hambiGomb" style={{ marginLeft: sidebarOpen ? '25%' : '0',}}>
