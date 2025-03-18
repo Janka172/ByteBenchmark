b7ae42684bf82946f99bcf9e7da42f1b7d8c8515
Janka172 <jankahegedus872@gmail.com>
2025-03-18 11:03:12 +0100

    250318

--- Frontend/src/sajatSetup/SetupBeallitasok.jsx
+++ Frontend/src/sajatSetup/SetupBeallitasok.jsx
@@ -7,9 +7,11 @@ function SetupBeallitasok() {
     const [betoltSS, setBetoltSS] = useState(true);
     const [Mind, setMind] = useState([]);
     const [ReszletSzoveg, setReszletSzoveg] = useState([]);
+    const [ModSzoveg, setModSzoveg] = useState([]);
 
     const [tablazatDisp, setTablazatDisp] = useState('grid');
     const [reszletDisp, setReszletDisp] = useState('none');
+    const [modDisp, setModDisp] = useState('none');
 
     const [profilUrl, setProfilUrl] = useState('');
     useEffect(() => {
@@ -49,7 +51,7 @@ function SetupBeallitasok() {
                 <div className='setupTR' key={elem.Gepigeny}>
                     <div className='setupTD'>{elem.Gepigeny.split('.')[1]}</div>
                     <div className='setupTD gombTD' onClick={() => reszletekMegjelenitese(elem.Gepigeny.split('.')[1])}>Részletek Megjelenítése</div>
-                    <div className='setupTD gombTD'>Módosítás</div>
+                    <div className='setupTD gombTD' onClick={() => modositasMegjelenitese(elem.Gepigeny.split('.')[1])}>Módosítás</div>
                 </div>
             )
         }
@@ -57,52 +59,84 @@ function SetupBeallitasok() {
     }
 
     async function reszletekMegjelenitese(setupNeve){
-        console.log(setupNeve)
-
         setTablazatDisp('none');
         setReszletDisp('grid');
 
         let kivalasztottSetup = sajatSetup.find(x => setupNeve==x.Gepigeny.split('.')[1])
-        console.log(kivalasztottSetup)
         let ujReszletSzoveg = [];
         ujReszletSzoveg.push(
-            <div className='reszletRacs'>
-                <div className='reszletSor'>
-                    <div>Alaplap:</div>
-                    <div>{kivalasztottSetup.AlaplapNeve}</div>
-                    <Link to='/oldalak/AlkatreszReszletek' state={{'tipus':'a', 'id': await getAlaplap(kivalasztottSetup.AlaplapNeve)}}><button className='tovabiReszletekGomb'>További részletek</button></Link>
+            <div className='reszletRacs' key={`reszletRacs-${kivalasztottSetup.AlaplapNeve}`}>
+                <div className='reszletSor' key={`aS-${kivalasztottSetup.AlaplapNeve}`}>
+                    <div key={`aC-${kivalasztottSetup.AlaplapNeve}`}>Alaplap:</div>
+                    <div key={`aSz-${kivalasztottSetup.AlaplapNeve}`}>{kivalasztottSetup.AlaplapNeve}</div>
+                    <Link to='/oldalak/AlkatreszReszletek' state={{'tipus':'a', 'id': await getAlaplap(kivalasztottSetup.AlaplapNeve)}}>
+                        <button className='tovabiReszletekGomb'>További részletek</button>
+                    </Link>
                 </div>
-
-                <div className='reszletSor'>
-                    <div>Processzor:</div>
-                    <div>{kivalasztottSetup.ProcesszorNev}</div>
-                    <Link to='/oldalak/AlkatreszReszletek' state={{'tipus':'p', 'id': await getProcesszor(kivalasztottSetup.ProcesszorNev)}}><button className='tovabiReszletekGomb'>További részletek</button></Link>
+        
+                <div className='reszletSor' key={`pS-${kivalasztottSetup.ProcesszorNev}`}>
+                    <div key={`pC-${kivalasztottSetup.ProcesszorNev}`}>Processzor:</div>
+                    <div key={`pSz-${kivalasztottSetup.ProcesszorNev}`}>{kivalasztottSetup.ProcesszorNev}</div>
+                    <Link to='/oldalak/AlkatreszReszletek' state={{'tipus':'p', 'id': await getProcesszor(kivalasztottSetup.ProcesszorNev)}}>
+                        <button className='tovabiReszletekGomb'>További részletek</button>
+                    </Link>
                 </div>
-
-                <div className='reszletSor'>
-                    <div>Videókártya:</div>
-                    <div>{kivalasztottSetup.VidekortyaNev} - {kivalasztottSetup.VideokartyaVram}GB</div>
-                    <Link to='/oldalak/AlkatreszReszletek' state={{'tipus':'v', 'id': await getVideokartya(kivalasztottSetup.VidekortyaNev, kivalasztottSetup.VideokartyaVram)}}><button className='tovabiReszletekGomb'>További részletek</button></Link>
+        
+                <div className='reszletSor' key={`vS-${kivalasztottSetup.VidekortyaNev}`}>
+                    <div key={`vC-${kivalasztottSetup.VidekortyaNev}`}>Videókártya:</div>
+                    <div key={`vSz-${kivalasztottSetup.VidekortyaNev}`}>{kivalasztottSetup.VidekortyaNev} - {kivalasztottSetup.VideokartyaVram}GB</div>
+                    <Link to='/oldalak/AlkatreszReszletek' state={{'tipus':'v', 'id': await getVideokartya(kivalasztottSetup.VidekortyaNev, kivalasztottSetup.VideokartyaVram)}}>
+                        <button className='tovabiReszletekGomb'>További részletek</button>
+                    </Link>
                 </div>
-
-                <div className='reszletSor'>
-                    <div>Ram:</div>
-                    <div>{kivalasztottSetup.RamNeve} - {kivalasztottSetup.RamFrekvencia}Hz</div>
-                    <Link to='/oldalak/AlkatreszReszletek' state={{'tipus':'r', 'id': await getRam(kivalasztottSetup.RamNeve, kivalasztottSetup.RamFrekvencia, kivalasztottSetup.RamMeret)}}><button className='tovabiReszletekGomb'>További részletek</button></Link>
+        
+                <div className='reszletSor' key={`rS-${kivalasztottSetup.RamNeve}`}>
+                    <div key={`rC-${kivalasztottSetup.RamNeve}`}>Ram:</div>
+                    <div key={`rSz-${kivalasztottSetup.RamNeve}`}>{kivalasztottSetup.RamNeve} - {kivalasztottSetup.RamFrekvencia}Hz</div>
+                    <Link to='/oldalak/AlkatreszReszletek' state={{'tipus':'r', 'id': await getRam(kivalasztottSetup.RamNeve, kivalasztottSetup.RamFrekvencia, kivalasztottSetup.RamMeret)}}>
+                        <button className='tovabiReszletekGomb'>További részletek</button>
+                    </Link>
                 </div>
-
-                <div className='reszletSor'>
-                    <div>Operációsrendszer:</div>
-                    <div>{kivalasztottSetup.OprendszerNev}</div>
-                    <Link to='/oldalak/AlkatreszReszletek' state={{'tipus':'r', 'id': await getOpRendszer(kivalasztottSetup.OprendszerNev)}}><button className='tovabiReszletekGomb'>További részletek</button></Link>
+        
+                <div className='reszletSor' key={`oS-${kivalasztottSetup.OprendszerNev}`}>
+                    <div key={`oC-${kivalasztottSetup.OprendszerNev}`}>Operációsrendszer:</div>
+                    <div key={`oSz-${kivalasztottSetup.OprendszerNev}`}>{kivalasztottSetup.OprendszerNev}</div>
+                    <Link to='/oldalak/AlkatreszReszletek' state={{'tipus':'r', 'id': await getOpRendszer(kivalasztottSetup.OprendszerNev)}}>
+                        <button className='tovabiReszletekGomb'>További részletek</button>
+                    </Link>
                 </div>
             </div>
-        )
+        );
+        
 
         setReszletSzoveg(ujReszletSzoveg);
     }
 
-    //Kiválasztások
+    async function modositasMegjelenitese(setupNeve){
+        setTablazatDisp('none');
+        setModDisp('grid');
+
+        let kivalasztottSetup = sajatSetup.find(x => setupNeve==x.Gepigeny.split('.')[1]);
+        let alaplapok = await getMindenAlaplap(kivalasztottSetup.AlaplapNeve);
+        
+        console.log(alaplapok)
+
+        let ujModSzoveg = [];
+        ujModSzoveg.push(
+            <div className='modRacs'>
+                <div className='modRacs'>
+                    <div>Alaplap</div>
+                    <select >{alaplapok.map(x => (
+                        <option value={x.Nev} key={x.Nev}>{x.Nev}</option>
+                    ))}</select>
+                </div>
+
+            </div>
+        )
+        setModSzoveg(ujModSzoveg);
+    }
+
+    //Kiválasztott adatok lekérése
     async function getAlaplap(kivAlaplapNeve) {
         try {
             const response = await fetch(`https://localhost:44316/api/Alaplap/0?name=${kivAlaplapNeve}`);
@@ -125,8 +159,6 @@ function SetupBeallitasok() {
 
     async function getVideokartya(kivVidkNev, kivVidkVram) {
         try {
-            console.log(kivVidkNev)
-            console.log(kivVidkVram)
           const response = await fetch(`https://localhost:44316/api/Videokartya/0?name=${kivVidkNev}&vram=${kivVidkVram}`);
           const data = await response.json();
           return data;
@@ -155,6 +187,19 @@ function SetupBeallitasok() {
         }
     }
 
+    //Minden adat lekérése
+    async function getMindenAlaplap(kivAlaplapNeve) {
+        try {
+            const response = await fetch(`https://localhost:44316/api/Alaplap`);
+            const data = await response.json();
+            data.find(x => {
+                if(x.Nev==kivAlaplapNeve) return x;
+            }).Nev='-';
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
   return (
     <div>
@@ -168,7 +213,10 @@ function SetupBeallitasok() {
         <div className='setupReszletek' style={{ display: reszletDisp }}>
             <div className='setVissza' onClick={alaphelyzetbe}>Vissza</div>
             {ReszletSzoveg}
-
+        </div>
+        <div className='setupModositas' style={{display: modDisp}}>
+            <div className='setVissza' onClick={alaphelyzetbe}>Vissza</div>
+            {ModSzoveg}
         </div>
     </div>
   );
